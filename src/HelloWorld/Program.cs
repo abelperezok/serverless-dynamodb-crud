61b6cba48a5ab9f3c1606dc920a3a2c956889a59
Abel Perez Martinez <abel@ABEL-DESKTOP>
2018-07-23 23:30:31 +0100

    removing dead code

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -4,10 +4,8 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
-
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
-using AwsDynamoDb;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
